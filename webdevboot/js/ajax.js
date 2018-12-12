da974154bc0d0c6e5c7c1165d2067407368035f8
Vadim Hvozd <vadimhvozd@MacBook-Pro-Vadim-2.local>
2018-12-12 12:43:43 +0200

    boot

---
+++ webdevboot/js/ajax.js
@@ -0,0 +1,28 @@
+/* Article FructCode.com */
+$( document ).ready(function() {
+    $("#btn").click(
+		function(){
+			sendAjaxForm('result_form', 'ajax_form', 'action_ajax_form.php');
+			return false;
+		}
+	);
+});
+
+function sendAjaxForm(result_form, ajax_form, url) {
+    $.ajax({
+        url:     'https://webdepartament.com/send.php', //url страницы (action_ajax_form.php)
+        type:     "POST", //метод отправки
+        dataType: "JSON", //формат данных
+        data: $("#"+ajax_form).serialize(),  // Сеарилизуем объект
+        success: function(response) { //Данные отправлены успешно
+        	result = $.parseJSON(response);
+        	$('#result_form').html('Имя: '+result.name+'<br>Телефон: '+result.phonenumber);
+    	},
+    	error: function(response) { // Данные не отправлены
+            $('#result_form').html('Ошибка. Данные не отправленны.');
+    	}
+ 	});
+}
+
+
+
---
+++ webdevboot/php/action_ajax_form.php
@@ -0,0 +1,15 @@
+<?php
+/* Article FructCode.com */
+if (isset($_POST["name"]) && isset($_POST["phonenumber"]) ) { 
+
+	// Формируем массив для JSON ответа
+    $result = array(
+    	'name' => $_POST["name"],
+    	'phonenumber' => $_POST["phonenumber"]
+    ); 
+
+    // Переводим массив в JSON
+    echo json_encode($result); 
+}
+
+?>
